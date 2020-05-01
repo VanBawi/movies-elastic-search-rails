488253538935eef75989115d89342b7db25776ed
Jack Van <superjackpower85@gmail.com>
2020-05-01 21:17:03 +0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :movies do
+    collection do 
+      get :autocomplete
+    end
+  end
+
+  root :to =>'movies#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
